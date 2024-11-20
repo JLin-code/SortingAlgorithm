974072b9fee9d2ff6dc2af9e3002e0ae047e0a50
Jack Lin <lin_jack@myyahoo.com>
2024-11-20 17:12:12 -0500

    index done style 50%

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import SortingVisualizer from './SortingVisualizer/SortingVisualizer'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <SortingVisualizer></SortingVisualizer>
     </div>
   );
 }
