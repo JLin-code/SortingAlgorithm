f1ccc2ce00201445050106ef24e7afbb4181d73c
Jack Lin <lin_jack@myyahoo.com>
2024-11-20 17:43:20 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -44,3 +44,44 @@ const start = () => {
   }
 }
 
+start()
+
+async function swapBars(barsDivs, i, j) {
+  barsDivs[i].style.left = `${5 + j * 30}px`
+  barsDive[i].classList.add('activate')
+  barsDivs[j].style.left = `${5 + i * 30}px`
+  barsDive[j].classList.add('activate')
+  await sleep(300)
+  bars.Divs[i].classList.remove('activate')
+  barsDivs[j].classList.remove('activate')
+  let temp = barsDivs[i]
+  barsDivs[i] = barsDivs[j]
+  barsDivs[j] = temp
+}
+
+const algorithms = [
+  sortingAlgorithms.bubbleSort
+]
+
+const solve = async () => {
+  const algorithm = structuredClone(bars.map(el => el.height));
+
+  const swaps = algorithms[selectAlgorithm.selectedIndex](array);
+
+  for (let i = 0; i < swaps.length; i++) {
+    if(swaps[i].firstPosition === swaps[i].lastPosition) {
+      barsDivs[swaps[i].firstPosition].classList.add('activate')
+      await swapBars(barsDivs, swaps[i].firstPosition, swaps[i].lastPosition)
+    }
+  }
+}
+
+generateBtn.addEventListener('click', () => {
+  nBars = parseInt(numberBars.ariaValueMax, 10);
+  stage.style.width = `${nBars * 30}px`;
+  start()
+})
+
+solveBtn,addEventListener('click', () => {
+  solve()
+})
